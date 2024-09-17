a436132aa07535595e2c1ad46bb527d962599e06
kishoreECS <kishorekumar.ravi04@gmail.com>
2024-09-17 12:57:01 +0530

    banner on process

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,25 @@
 console.log("Function is working")
+$(document).ready(function () {
+    $(".carousel_slider").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+      navText: ["&#10094;", "&#10095;"],
+    });
+  });
+  
