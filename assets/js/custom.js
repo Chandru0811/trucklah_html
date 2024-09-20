d8aa5b16f39d1e51197aeab0d8e9dbe13c5ae993
Dollali <sangeethaecs2000@gmail.com>
2024-09-20 16:56:41 +0530

    bug fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,20 +2,23 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
-    nav: false, 
-    dots: true, 
-    autoplay: false, 
-    autoplayTimeout: 5000, 
+    nav: false,
+    dots: true,
+    autoplay: false,
+    autoplayTimeout: 5000,
     autoplayHoverPause: true,
     responsive: {
       0: {
         items: 1,
       },
-      600: {
+      768: {
         items: 2,
       },
       1000: {
         items: 3,
+      },
+      1500: {
+        items: 4,
       },
     },
   });
